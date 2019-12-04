32a5885b063571e83bb81549f84e10b388f0fb4c
Gavin Daly <408519+gavdaly@users.noreply.github.com>
2019-12-04 06:19:33 -0500

    Theme working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    title: `GavDaly`,
+    name: `GavDaly`,
+    siteUrl: `https://gavdaly.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
       heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
@@ -11,38 +11,182 @@ module.exports = {
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/gavdaly`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/gavdaly`,
       },
       {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://instagram.com/gavdaly`,
       },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
+      // {
+      //   name: `linkedin`,
+      //   url: `https://www.linkedin.com/company/narative/`,
+      // },
     ],
   },
+  pathPrefix: `/`,
+  mapping: {
+    "Mdx.frontmatter.author": `AuthorsYaml`,
+  },
   plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-yaml`,
+    `gatsby-plugin-theme-ui`,
     {
-      resolve: "@narative/gatsby-theme-novela",
+      resolve: `gatsby-plugin-feed`,
       options: {
-        contentPosts: "content/posts",
-        contentAuthors: "content/authors",
-        basePath: "/",
-        authorsPage: true,
-        sources: {
-          local: true,
-          // contentful: true,
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        setup: ({
+          query: {
+            site: { siteMetadata },
+          },
+          ...rest
+        }) => {
+          siteMetadata.feed_url = siteMetadata.siteUrl + "/rss.xml"
+          siteMetadata.image_url =
+            siteMetadata.siteUrl + "/icons/icon-512x512.png"
+          const siteMetadataModified = siteMetadata
+          siteMetadataModified.feed_url = `${siteMetadata.siteUrl}/rss.xml`
+          siteMetadataModified.image_url = `${siteMetadata.siteUrl}/icons/icon-512x512.png`
+
+          return {
+            ...siteMetadataModified,
+            ...rest,
+          }
         },
+        feeds: [
+          {
+            serialize: ({ query: { site, allArticle, allContentfulPost } }) => {
+              if (local && !contentful) {
+                return allArticle.edges
+                  .filter(edge => !edge.node.secret)
+                  .map(edge => {
+                    return {
+                      ...edge.node,
+                      description: edge.node.excerpt,
+                      date: edge.node.date,
+                      url: site.siteMetadata.siteUrl + edge.node.slug,
+                      guid: site.siteMetadata.siteUrl + edge.node.slug,
+                      // custom_elements: [{ "content:encoded": edge.node.body }],
+                      author: edge.node.author,
+                    }
+                  })
+              } else if (!local && contentful) {
+                return allContentfulPost.edges
+                  .filter(edge => !edge.node.secret)
+                  .map(edge => {
+                    return {
+                      ...edge.node,
+                      description: edge.node.excerpt,
+                      date: edge.node.date,
+                      url: site.siteMetadata.siteUrl + edge.node.slug,
+                      guid: site.siteMetadata.siteUrl + edge.node.slug,
+                      // custom_elements: [{ "content:encoded": edge.node.body }],
+                      author: edge.node.author,
+                    }
+                  })
+              } else {
+                const allArticlesData = { ...allArticle, ...allContentfulPost }
+                return allArticlesData.edges
+                  .filter(edge => !edge.node.secret)
+                  .map(edge => {
+                    return {
+                      ...edge.node,
+                      description: edge.node.excerpt,
+                      date: edge.node.date,
+                      url: site.siteMetadata.siteUrl + edge.node.slug,
+                      guid: site.siteMetadata.siteUrl + edge.node.slug,
+                      // custom_elements: [{ "content:encoded": edge.node.body }],
+                      author: edge.node.author,
+                    }
+                  })
+              }
+            },
+            query: `
+              {
+                allArticle(sort: {order: DESC, fields: date}) {
+                  edges {
+                    node {
+                      excerpt
+                      date
+                      slug
+                      title
+                      author
+                      secret
+                    }
+                  }
+                }
+              }`,
+            output: "/rss.xml",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `content/posts`,
+        name: `content/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `content/authors`,
+        name: `content/authors`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 10000,
+              linkImagesToOriginal: false,
+              quality: 80,
+              withWebp: true,
+            },
+          },
+          { resolve: `gatsby-remark-copy-linked-files` },
+          { resolve: `gatsby-remark-numbered-footnotes` },
+          { resolve: `gatsby-remark-smartypants` },
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "noreferrer", // eslint-disable-line unicorn/prevent-abbreviations
+            },
+          },
+        ],
+        remarkPlugins: [require(`remark-slug`)], // eslint-disable-line global-require
+      },
+    },
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        displayName: process.env.NODE_ENV === `development`,
       },
     },
     {
@@ -58,4 +202,4 @@ module.exports = {
       },
     },
   ],
-};
+}
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const About: React.FC = () => {
+  return <h1>ABOUT</h1>
+}
+
+export default About
