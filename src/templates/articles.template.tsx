f9a40f82626d971a220e0fb9f3954b6535e6d832
Gavin Daly <408519+gavdaly@users.noreply.github.com>
2019-12-04 11:22:40 -0500

    Remove Gradents at the bottom of the page

--- src/templates/articles.template.tsx
+++ src/templates/articles.template.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from "react"
+import styled from "@emotion/styled"
 
-import Section from "@components/Section";
-import SEO from "@components/SEO";
-import Layout from "@components/Layout";
-import Paginator from "@components/Navigation/Navigation.Paginator";
+import Section from "@components/Section"
+import SEO from "@components/SEO"
+import Layout from "@components/Layout"
+import Paginator from "@components/Navigation/Navigation.Paginator"
 
-import ArticlesHero from "../sections/articles/Articles.Hero";
-import ArticlesList from "../sections/articles/Articles.List";
+import ArticlesHero from "../sections/articles/Articles.Hero"
+import ArticlesList from "../sections/articles/Articles.List"
 
-import { Template } from "@types";
+import { Template } from "@types"
 
 const ArticlesPage: Template = ({ location, pageContext }) => {
-  const articles = pageContext.group;
-  const authors = pageContext.additionalContext.authors;
+  const articles = pageContext.group
+  const authors = pageContext.additionalContext.authors
 
   return (
     <Layout>
@@ -25,25 +25,12 @@ const ArticlesPage: Template = ({ location, pageContext }) => {
           <Paginator {...pageContext} />
         </ArticlesPaginator>
       </Section>
-      <ArticlesGradient />
     </Layout>
-  );
-};
+  )
+}
 
-export default ArticlesPage;
-
-const ArticlesGradient = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 590px;
-  z-index: 0;
-  pointer-events: none;
-  background: ${p => p.theme.colors.gradient};
-  transition: ${p => p.theme.colorModeTransition};
-`;
+export default ArticlesPage
 
 const ArticlesPaginator = styled.div<{ show: boolean }>`
   ${p => p.show && `margin-top: 95px;`}
-`;
+`
