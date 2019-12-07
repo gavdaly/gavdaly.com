5bf4c57acc4802e18d1a1532d3366d3c2af456f3
Gavin Daly <408519+gavdaly@users.noreply.github.com>
2019-12-07 13:37:12 -0500

    added grid to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import { Global } from '@emotion/core';
-import styled from '@emotion/styled';
-import { useColorMode } from 'theme-ui';
+import React, { useEffect } from 'react'
+import { Global } from '@emotion/core'
+import styled from '@emotion/styled'
+import { useColorMode } from 'theme-ui'
 
-import NavigationFooter from '@components/Navigation/Navigation.Footer';
-import NavigationHeader from '@components/Navigation/Navigation.Header';
-import ArticlesContextProvider from '../../sections/articles/Articles.List.Context';
+import NavigationFooter from '@components/Navigation/Navigation.Footer'
+import NavigationHeader from '@components/Navigation/Navigation.Header'
+import ArticlesContextProvider from '../../sections/articles/Articles.List.Context'
 
-import { globalStyles } from '@styles';
+import { globalStyles } from '@styles'
 
 /**
  * <Layout /> needs to wrap every page as it provides styles, navigation,
@@ -15,29 +15,43 @@ import { globalStyles } from '@styles';
  * which hides a lot of the mess we need to create our Desktop and Mobile experiences.
  */
 const Layout: React.FC<{}> = ({ children }) => {
-  const [colorMode] = useColorMode();
+  const [colorMode] = useColorMode()
 
   useEffect(() => {
-    parent.postMessage({ theme: colorMode }, '*');
-  }, [colorMode]);
+    parent.postMessage({ theme: colorMode }, '*')
+  }, [colorMode])
 
   return (
-    <ArticlesContextProvider >
+    <ArticlesContextProvider>
       <Container id={colorMode}>
         <Global styles={globalStyles} />
-        <NavigationHeader />
-        {children}
-        <NavigationFooter />
+        <NavigationHeader id="header" />
+        <main id="main">{children}</main>
+        <NavigationFooter id="footer" />
       </Container>
     </ArticlesContextProvider>
-  );
+  )
 }
 
-export default Layout;
+export default Layout
 
 const Container = styled.div`
-  position: relative;
   background: var(--color-background);
   transition: background 0.25s var(--ease-in-out-quad), color 0.25s var(--ease-in-out-quad);
   min-height: 100vh;
-`;
+  display: grid;
+  grid-template-areas:
+    'header'
+    'main'
+    'footer';
+
+  #header {
+    grid-area: header;
+  }
+  #main {
+    grid-area: main;
+  }
+  #footer {
+    grid-area: footer;
+  }
+`
