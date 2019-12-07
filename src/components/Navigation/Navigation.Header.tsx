52cb868553b814b040e8effe8591c24cbde49913
Gavin Daly <408519+gavdaly@users.noreply.github.com>
2019-12-07 08:26:12 -0500

    use currentColor to gert rid of ternary in CSS

--- src/components/Navigation/Navigation.Header.tsx
+++ src/components/Navigation/Navigation.Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import styled from "@emotion/styled"
-import { Link, navigate, graphql, useStaticQuery } from "gatsby"
+import { Link, navigate } from "gatsby"
 import { useColorMode } from "theme-ui"
 
 import Section from "../../components/Section"
@@ -13,6 +13,8 @@ import {
   getWindowDimensions,
   getBreakpointFromTheme,
 } from "../../utils"
+
+import { ToolTip } from "../ToolTip"
 
 const DarkModeToggle: React.FC<{}> = () => {
   const [colorMode, setColorMode] = useColorMode()
@@ -203,33 +205,6 @@ const NavControls = styled.div`
   ${mediaqueries.phablet`
     right: -5px;
   `}
-`
-
-const ToolTip = styled.div<{ isDark: boolean; hasCopied: boolean }>`
-  position: absolute;
-  padding: 4px 13px;
-  background: ${p => (p.isDark ? "#000" : "rgba(0,0,0,0.1)")};
-  color: ${p => (p.isDark ? "#fff" : "#000")};
-  border-radius: 5px;
-  font-size: 14px;
-  top: -35px;
-  opacity: ${p => (p.hasCopied ? 1 : 0)};
-  transform: ${p => (p.hasCopied ? "translateY(-3px)" : "none")};
-  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
-
-  &::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: -6px;
-    margin: 0 auto;
-    width: 0;
-    height: 0;
-    border-left: 6px solid transparent;
-    border-right: 6px solid transparent;
-    border-top: 6px solid ${p => (p.isDark ? "#000" : "rgba(0,0,0,0.1)")};
-  }
 `
 
 const IconWrapper = styled.button<{ isDark: boolean }>`
