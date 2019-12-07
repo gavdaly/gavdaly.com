e6a09211b82edd9a7f4ce7206a963d1df6d9cc26
Gavin Daly <408519+gavdaly@users.noreply.github.com>
2019-12-07 08:26:50 -0500

    move tooltip to it's own file

---
+++ src/components/ToolTip.tsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled"
+
+export const ToolTip = styled.div<{
+    isDark: boolean;
+    hasCopied: boolean;
+}> `
+  position: absolute;
+  padding: 4px 13px;
+  background: ${p => (p.isDark ? "#000" : "rgba(0,0,0,0.1)")};
+  color: ${p => (p.isDark ? "#fff" : "#000")};
+  border-radius: 5px;
+  font-size: 14px;
+  top: -35px;
+  opacity: ${p => (p.hasCopied ? 1 : 0)};
+  transform: ${p => (p.hasCopied ? "translateY(-3px)" : "none")};
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: -6px;
+    margin: 0 auto;
+    width: 0;
+    height: 0;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    border-top: 6px solid ${p => (p.isDark ? "#000" : "rgba(0,0,0,0.1)")};
+  }
+`
