5ad40ae1c8704503774f7a3c2309ec1831f4d1d3
Gavin Daly <408519+gavdaly@users.noreply.github.com>
2019-12-07 11:05:35 -0500

    Speed up loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
-exports.onInitialClientRender = require('./src/gatsby/browser/onInitialClientRender');
-exports.onRouteUpdate = require('./src/gatsby/browser/onRouteUpdate');
-exports.shouldUpdateScroll = require('./src/gatsby/browser/shouldUpdateScroll');
+// exports.onInitialClientRender = require('./src/gatsby/browser/onInitialClientRender');
+// exports.onRouteUpdate = require('./src/gatsby/browser/onRouteUpdate');
+// exports.shouldUpdateScroll = require('./src/gatsby/browser/shouldUpdateScroll');
